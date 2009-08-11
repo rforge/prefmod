2ec79597442beaaea1ca07c3df850fbd24bcba16
hatz <hatz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-11 16:54:31 +0000

    depricated
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/prefmod@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/prefmod/R/print.pattW.R
+++
@@ -1,8 +0,0 @@
-print.pattW<-function(x,...)
-{
-    x<-unclass(x)
-    cat("\nWorthmatrix:\n")
-    if(ncol(x)>1) cat("\n")  # blank line only if there are groups
-    print(round(x,digits=4))
-    cat("\n")
-}
