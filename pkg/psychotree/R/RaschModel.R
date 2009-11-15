5e4494a3fb8dde15294dd97fec728abcd331d186
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-15 20:42:37 +0000

    first stab at Rasch tree models
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/prefmod@78 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/psychotree/R/RaschModel.R
@@ -0,0 +1,312 @@
+## S4 StatModel object
+RaschModel <- function(gradtol = 1e-12, hessian = FALSE) {
+  new("StatModel",
+    capabilities = new("StatModelCapabilities"),
+    name = "Rasch model",
+    dpp = ModelEnvFormula,
+    fit = function(object, weights = NULL, ...){
+
+        ## extract response (there are no regressors)
+        y <- object@get("response")[[1]]
+
+        ## call RaschModel.fit()
+        z <- RaschModel.fit(y = y, weights = weights,
+          gradtol = gradtol, hessian = hessian)
+        z$ModelEnv <- object
+        z$addargs <- list(...)
+        z
+    }
+  )
+}
+
+## methods needed for mob()
+reweight.RaschModel <- function(object, weights, ...) {
+     fit <- RaschModel(gradtol = object$gradtol, hessian = object$hessian)@fit
+     do.call("fit", c(list(object = object$ModelEnv, weights = weights), object$addargs))
+}
+
+
+print.RaschModel <- function(x, digits = max(3, getOption("digits") - 3), ...) {
+  cat("Rasch model coefficients:\n")
+  print(coef(x), digits = digits)
+  invisible(x)
+}
+
+## workhorse fitting function
+RaschModel.fit <- function(y, weights = NULL, start = NULL,
+  gradtol = 1e-12, check.analyticals = FALSE, hessian = FALSE, ...)
+{
+  ## original data
+  y <- as.matrix(y)
+  k <- ncol(y)
+  n <- nrow(y)
+  nam <- dimnames(y)
+  ## need at least three items
+  stopifnot(k >= 3)
+
+  ## weights processing
+  if(is.null(weights)) weights <- rep.int(1L, n)
+  ## data and weights need to match
+  stopifnot(length(weights) == n)
+
+  ## ## could try to condense data
+  ## ychar <- apply(y, 1, paste, collapse = "\r")
+  ## ydup <- duplicated(ychar)
+  ## if(any(ydup)) {
+  ##   y <- y[!ydup,, drop = FALSE]
+  ##   weights <- as.vector(tapply(weights, factor(ychar, levels = ychar[!ydup]), sum))
+  ## }
+  
+  ## data statistics (FIXME: na.rm?)
+  cs <- colSums(y * weights)
+  rs <- rowSums(y)
+  rf <- as.vector(tapply(weights, factor(rs, levels = 0:k), sum))
+  rf[is.na(rf)] <- 0
+
+  ## starting values (FIXME: boundary cases?)
+  if(is.null(start)) {
+    sw <- sum(weights)
+    start <- qlogis(ifelse(cs < 1, 1, ifelse(cs >= sw, sw-1, cs))/sw)
+  }
+  
+  ## contrast: set parameter 1 to zero
+  start <- start[-1] - start[1]
+  rf <- rf[-1]
+  cs <- cs[-1]
+  
+  ## objective function: conditional log-likelihood
+  cloglik <- function(par) {
+    ## obtain esf and apply contrast
+    esf <- elemenatry_symmetric_functions(c(0, par), order = 1)
+    g <- esf[[1]][-1]
+    g1 <- esf[[2]][-1, -1]
+
+    ## conditional log-likelihood
+    cll <- sum(cs * par) - sum(rf * log(g))
+    ## gradient
+    grad <- cs - colSums(rf * g1/g)
+
+    ## collect and return
+    attr(cll, "gradient") <- - grad
+    return(-cll)
+  }
+  
+  ## analytical hessian
+  ahessian <- function(par, esf = NULL) {
+    ## obtain esf and apply contrast
+    if(is.null(esf)) esf <- elemenatry_symmetric_functions(c(0, par), order = 2)
+    g <- esf[[1]][-1]
+    g1 <- esf[[2]][-1, -1]
+    g2 <- esf[[3]][-1, -1, -1]
+
+    ## hessian
+    hess <- matrix(0, ncol = k-1, nrow = k-1)
+    g1s <- g1/g
+    for (q in 1:(k-1)) hess[q,] <- colSums(rf * (g2[,q,]/g - (g1[,q]/g) * g1s))
+    
+    return(hess)
+  }
+  
+  ## optimization
+  opt <- nlm(cloglik, start,
+    hessian = hessian, gradtol = gradtol, check.analyticals = check.analyticals)
+  
+  ## collect and annotate results
+  cf <- opt$estimate
+  esf <- elemenatry_symmetric_functions(c(0, cf), order = 2)
+  vc <- if(hessian) opt$hessian else ahessian(cf, esf)
+  vc <- solve(vc)
+  names(cf) <- rownames(vc) <- colnames(vc) <- colnames(y)[-1]  
+  
+  ## collect, class, and return
+  rval <- list(
+    coefficients = cf,
+    vcov = vc,
+    loglik = -opt$minimum,
+    df = k-1,
+    weights = weights,
+    n = n,
+    data = y,
+    dimnames = nam,
+    elemenatry_symmetric_functions = esf,
+    nlm_code = opt$code,
+    iterations = opt$iterations,
+    hessian = hessian,
+    gradtol = gradtol
+  )
+  class(rval) <- "RaschModel"
+  return(rval)
+}
+
+## elementary symmetric functions
+elemenatry_symmetric_functions <- function(par, order = 0, log = TRUE) {
+  ## Michelle Liou (1994). More on the Computation of Higher-Order
+  ## Derivatives of the Elementary Symmetric Functions in the
+  ## Rasch Model. Applied Psychological Measurement, 18(1), 53-62.
+
+  ## derivatives up to order
+  order <- round(order)[1]
+  stopifnot(order %in% 0:2)
+  rval <- list()[1:(order+1)]
+  names(rval) <- 0:order
+
+  ## transformations
+  par <- as.vector(par)
+  beta <- if(log) par else log(par)
+  eps <- exp(beta)
+  n <- length(eps)
+  stopifnot(n > 2)
+  
+  ## Order: 0  
+  ## initialization: gamma_1(eps_1) = eps_1
+  gamma <- c(eps[1], numeric(n-1))
+
+  ## recursion: Equation 3
+  for(i in 2:n) gamma[1:i] <- c(eps[i] + gamma[1],
+    eps[i] * gamma[(2:i) - 1] + gamma[2:i])
+  
+  ## gamma_0 = 1
+  gamma <- c(1, gamma)
+
+  ## return value
+  rval[[1]] <- gamma
+  if(order < 1) return(rval)
+
+  ## Order: 1
+  ## initialization: gamma_1^(j) = 1
+  gamma1 <- matrix(0, nrow = n+1, ncol = n)
+  gamma1[2,] <- 1
+  ## recursion: Equation 4
+  for(q in 3:(n+1)) gamma1[q,] <- gamma[q-1] - eps * gamma1[q-1,]
+  ## if input on log scale: include inner derivative
+  if(log) gamma1 <- t(t(gamma1) * eps)
+  ## return value
+  rval[[2]] <- gamma1
+  if(order < 2) return(rval)
+
+  ## Order: 2
+  ## initialization: gamma_2^(i,j) = 1
+  gamma2 <- array(0, c(n+1, n, n))
+  gamma2[3,,] <- 1
+  ## auxiliary variables
+  eps_plus_eps <- outer(eps, eps, "+")
+  eps_times_eps <- outer(eps, eps)
+  ## recursion: Jansen's Equation (Table 1, Forward, Second-Order)
+  for(q in 4:(n+1)) gamma2[q,,] <- gamma[q-2] -
+    (eps_plus_eps * gamma2[q-1,,] + eps_times_eps * gamma2[q-2,,])
+  ## if input on log scale: include inner derivative
+  if(log) for(q in 1:(n+1)) gamma2[q,,] <- eps_times_eps * gamma2[q,,]
+  ## each diagonal is simply first derivative
+  for(q in 2:(n+1)) diag(gamma2[q,,]) <- gamma1[q,]
+  ## return value
+  rval[[3]] <- gamma2
+  return(rval)
+}
+
+## methods
+coefficients.RaschModel <- function(object, ...) object$coefficients
+
+vcov.RaschModel <- function(object, ...) object$vcov
+
+logLik.RaschModel <- function(object, ...) structure(object$loglik, df = object$df, class = "logLik")
+
+bread.RaschModel <- function(x, ...) x$vcov * x$n
+
+estfun.RaschModel <- function(x, ...) {
+  g <- x$elemenatry_symmetric_functions[[1]][-1]
+  g1 <- x$elemenatry_symmetric_functions[[2]][-1,-1]
+  rs <- rowSums(x$data)
+  x$data[,-1] - g1[rs,] / g[rs]
+}
+
+worth.RaschModel <- function(object, ...) {
+  cf <- c(0, object$coefficients)
+  cf <- cf - mean(cf)
+  names(cf) <- colnames(object$data)
+  cf
+}
+
+summary.RaschModel <- function(object, vcov. = NULL, ...)
+{
+  ## coefficients
+  cf <- coef(object)
+
+  ## covariance matrix
+  if(is.null(vcov.)) 
+      vc <- vcov(object)
+  else {
+      if(is.function(vcov.)) vc <- vcov.(object)
+        else vc <- vcov.
+  }
+  
+  ## Wald test of each coefficient
+  cf <- cbind(cf, sqrt(diag(vc)), cf/sqrt(diag(vc)), 2 * pnorm(-abs(cf/sqrt(diag(vc)))))
+  colnames(cf) <- c("Estimate", "Std. Error", "z value", "Pr(>|z|)")
+
+  object$coefficients <- cf      
+  class(object) <- "summary.RaschModel"
+  return(object)
+}
+
+print.summary.RaschModel <- function(x, digits = max(3, getOption("digits") - 3), 
+    signif.stars = getOption("show.signif.stars"), ...)
+{
+  if(is.null(x$call)) {
+    cat("\nRasch model\n\n")  
+  } else {
+    cat("\nCall:\n")
+    cat(paste(deparse(x$call), sep = "\n", collapse = "\n"), "\n\n", sep = "")
+  }
+
+  cat("Parameters:\n")
+  printCoefmat(x$coefficients, digits = digits, signif.stars = signif.stars, na.print = "NA", ...)
+
+  cat("\nLog-likelihood:", format(signif(x$loglik, digits)),
+    "(df =", paste(x$df, ")", sep = ""), "\n")
+  cat("Number of iterations in nlm optimization:", x$iterations, "\n\n")
+  invisible(x)
+}
+
+plot.RaschModel <- function(x, 
+  center = TRUE, index = TRUE, names = TRUE, abbreviate = FALSE, ref = TRUE,
+  type = NULL, lty = NULL, xlab = "Items", ylab = NULL, ...)
+{
+  ## parameters to be plotted
+  cf <- worth(x)
+  if(!center) cf <- cf[-1]
+  cf_ref <- mean(cf)
+
+  ## labeling
+  if(is.character(names)) {
+    names(cf) <- names
+    names <- TRUE
+  }
+
+  if(is.null(ylab)) ylab <- if(center) "Centered parameters" else "Parameters"
+
+  ## abbreviation
+  if(is.logical(abbreviate)) {
+    nlab <- max(nchar(names(cf)))
+    abbreviate <- if(abbreviate) as.numeric(cut(nlab, c(-Inf, 1.5, 4.5, 7.5, Inf))) else nlab
+  }
+  names(cf) <- abbreviate(names(cf), abbreviate)
+
+  ## raw plot
+  ix <- if(index) seq(along = cf) else rep(0, length(cf))
+  plot(ix, cf, xlab = xlab, ylab = ylab, type = "n", axes = FALSE, ...)
+  if(ref) abline(h = cf_ref, col = "lightgray")
+  axis(2)
+  box()  
+
+  ## actual data
+  if(index) {
+    if(is.null(type)) type <- "b"
+    if(is.null(lty)) lty <- 2  
+    lines(ix, cf, type = type, lty = lty)
+    axis(1, at = ix, labels = if(names) names(cf) else TRUE)
+  } else {
+    if(is.null(type)) type <- "p"
+    if(names) text(names(cf), x = ix, y = cf, ...) else lines(ix, cf, type = type, lty = lty)
+  }
+}
+
---
+++ pkg/psychotree/R/raschtree.R
@@ -0,0 +1,143 @@
+## high-level convenience interface
+raschtree <- function(formula, data, na.action = na.pass, minsplit = 10, ...)
+{
+  ## transform formula
+  stopifnot(length(formula) > 2)
+  formula <-  formula(terms(formula, data = data))
+  ff <- y ~ 1 | x
+  ff[[2]] <- formula[[2]]
+  ff[[3]][[3]] <- formula[[3]]
+
+  ## call mob()
+  rval <- mob(ff, data = data, model = RaschModel(),
+    control = mob_control(minsplit = minsplit,
+      objfun = function(object) - as.vector(logLik(object)), ...), na.action = na.action)
+
+  ## add class and return
+  structure(list(mob = rval), class = "raschtree")
+}
+
+## convenience plotting
+plot.raschtree <- function(x, terminal_panel = node_raschplot, tnex = 2, ...) {
+  plot(x$mob, terminal_panel = terminal_panel, tnex = tnex, tp_args = list(...))
+}
+
+## hand-crafted "Next()" to bridge to
+## un-exported S4 classes "mob"/"BinaryTree", argh!
+logLik.raschtree <- function(object, ...) logLik(object$mob, ...)
+sctest.raschtree <- function(x, ...) sctest(x$mob, ...)
+weights.raschtree <- function(object, ...) weights(object$mob, ...)
+summary.raschtree <- function(object, ...) summary(object$mob, ...)
+print.raschtree <- function(x, ...) {
+  print(x$mob, ...)
+  invisible(x)
+}
+
+## parameters for Rasch trees
+coef.raschtree <- function (object, node = NULL, ...) 
+{
+  object <- object$mob
+  if(is.null(node)) node <- party:::terminal_nodeIDs(object@tree)
+  rval <- sapply(nodes(object, node), function(z) coef(z$model, ...))
+  if (!is.null(dim(rval))) {
+    rval <- t(rval)
+    rownames(rval) <- node
+  }
+  return(rval)
+}
+
+worth.raschtree <- function (object, node = NULL, ...) 
+{
+  object <- object$mob
+  if(is.null(node)) node <- party:::terminal_nodeIDs(object@tree)
+  rval <- sapply(nodes(object, node), function(z) worth(z$model, ...))
+  if (!is.null(dim(rval))) {
+    rval <- t(rval)
+    rownames(rval) <- node
+  }
+  return(rval)
+}
+
+## visualization function
+node_raschplot <- function(mobobj, id = TRUE,
+  center = TRUE, names = TRUE, abbreviate = TRUE, index = TRUE, ref = TRUE,
+  col = "black", linecol = "lightgray", cex = 0.5, pch = 19, xscale = NULL, yscale = NULL, ylines = 1.5)
+{
+    ## extract parameter of interest
+    node <- 1:max(party:::terminal_nodeIDs(mobobj@tree))
+    cf <- t(sapply(nodes(mobobj, node), function(z)
+      if(center) worth(z$model) else worth(z$model) - worth(z$model)[1]))
+    rownames(cf) <- node
+
+    ## labeling
+    if(is.character(names)) {
+      colnames(cf) <- names
+      names <- TRUE
+    }
+
+    ## abbreviation
+    if(is.logical(abbreviate)) {
+      nlab <- max(nchar(colnames(cf)))
+      abbreviate <- if(abbreviate) as.numeric(cut(nlab, c(-Inf, 1.5, 4.5, 7.5, Inf))) else nlab
+    }
+    colnames(cf) <- abbreviate(colnames(cf), abbreviate)
+    
+    if(index) {
+      x <- 1:NCOL(cf)
+      if(is.null(xscale)) xscale <- range(x) + c(-0.1, 0.1) * diff(range(x))
+    } else {
+      x <- rep(0, length(cf))
+      if(is.null(xscale)) xscale <- c(-1, 1)      
+    }
+    if(is.null(yscale)) yscale <- range(cf) + c(-0.1, 0.1) * diff(range(cf))
+         
+    ## panel function for bt plots in nodes
+    rval <- function(node) {
+    
+        ## dependent variable setup
+	cfi <- cf[node$nodeID,]
+	cf_ref <- mean(cfi)
+
+        ## viewport setup
+        top_vp <- viewport(layout = grid.layout(nrow = 2, ncol = 3,
+                           widths = unit(c(ylines, 1, 1), c("lines", "null", "lines")),  
+			   heights = unit(c(1, 1), c("lines", "null"))),
+                           width = unit(1, "npc"), 
+                           height = unit(1, "npc") - unit(2, "lines"),
+			   name = paste("node_raschplot", node$nodeID, sep = ""))
+        pushViewport(top_vp)
+        grid.rect(gp = gpar(fill = "white", col = 0))
+
+        ## main title
+        top <- viewport(layout.pos.col = 2, layout.pos.row = 1)
+        pushViewport(top)
+	mainlab <- paste(ifelse(id, paste("Node", node$nodeID, "(n = "), ""),
+	                 sum(node$weights), ifelse(id, ")", ""), sep = "")
+        grid.text(mainlab)
+        popViewport()
+
+        ## actual plot	
+        plot_vpi <- viewport(layout.pos.col = 2, layout.pos.row = 2,
+	    xscale = xscale, yscale = yscale, 
+	    name = paste("node_raschplot", node$nodeID, "plot", sep = ""))
+        pushViewport(plot_vpi)
+	
+        grid.lines(xscale, c(cf_ref, cf_ref), gp = gpar(col = linecol), default.units = "native")
+	if(index) {
+	  grid.lines(x, cfi, gp = gpar(col = col, lty = 2), default.units = "native")
+	  grid.points(x, cfi, gp = gpar(col = col, cex = cex), pch = pch, default.units = "native")
+	  grid.xaxis(at = x, label = if(names) names(cfi) else x)
+	} else {	  
+  	  if(names) grid.text(names(cfi), x = x, y = cfi, default.units = "native")
+	    else grid.points(x, cfi, gp = gpar(col = col, cex = cex), pch = pch, default.units = "native")
+	}
+        grid.yaxis(at = c(ceiling(yscale[1] * 100)/100, floor(yscale[2] * 100)/100))
+        grid.rect(gp = gpar(fill = "transparent"))
+
+	upViewport(2)
+    }
+	    
+    return(rval)
+}
+class(node_raschplot) <- "grapcon_generator"
+
