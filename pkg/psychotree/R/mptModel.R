fd13a2c19009a599d0b960d9366bf75806f6dcfe
fwickel <fwickel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-06 19:22:31 +0000

    S4 class for MPT models
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/prefmod@148 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/psychotree/R/mptModel.R
@@ -0,0 +1,56 @@
+## S4 StatModel object
+mptModel <- function(treeid = NULL, mptform = NULL, mptstruc = NULL) {
+  new("StatModel",
+    capabilities = new("StatModelCapabilities"),
+    name = "Multinomial processing tree model",
+    dpp = ModelEnvFormula,
+    fit = function(object, weights = NULL, ...){
+
+      ## extract response (there are no regressors)
+      y <- object@get("response")[[1]]
+
+      ## call btReg.fit()
+      z <- mptModel.fit(y = y, weights = weights, # treeid = treeid,
+                        mptform = mptform, mptstruc = mptstruc)
+      # z <- btReg.fit(y = y, weights = weights, type = type, ref = ref,
+      #   undecided = undecided, position = position)
+      z$ModelEnv <- object
+      z$addargs <- list(...)
+      z
+    }
+  )
+}
+
+
+reweight.mptModel <- function(object, weights, ...) {
+     fit <- mptModel(mptform = object$mptform,
+                     mptstruc = object$mptstruc)@fit
+     do.call("fit", c(list(object = object$ModelEnv, weights = weights), object$addargs))
+}
+
+
+estfun.mptModel <- function(x, ...) x$estfun
+
+
+# estfun.mptModel <- function(object, ...){
+#   y       <- object$y
+#   theta   <- coef(object)
+#   pcat    <- object$pcat
+#   sympcat <- parse(text = as.character(object$mptform[-1]))
+# 
+#   ## Symbolic derivative of sympcat for each theta
+#   dpcat <- matrix(NA, nrow=length(sympcat), ncol=length(theta))
+#   for(i in seq_along(sympcat))
+#     for(j in seq_along(theta))
+#       dpcat[i, j] <- eval(D(sympcat[i], names(theta)[j]), as.list(theta))
+# 
+#   # (Symbolic pcat evaluated at theta) == pcat
+#   # pcat <- sapply(sympcat, eval, as.list(theta))
+# 
+#   # Wide data
+#   t(sapply(seq_len(nrow(y)), function(i) colSums(y[i,]/pcat * dpcat)))
+# }
+
+
+bread.mptModel <- function(x, ...) vcov(x) * x$n
+
