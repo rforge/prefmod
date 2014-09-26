456e1e27b48c8f6535ac454bad677fd643e4c7d0
fwickel <fwickel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-09-26 15:17:36 +0000

    remove old mptModel glue, some docu
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/prefmod@252 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/psychotree/R/graphics.R
+++ pkg/psychotree/R/graphics.R
@@ -397,3 +397,99 @@ node_btplot <- function(mobobj, id = TRUE, worth = TRUE, names = TRUE,
     return(rval)
 }
 class(node_btplot) <- "grapcon_generator"
+
+
+## MPT plot visualization function
+node_mptplot <- function(mobobj, id = TRUE,
+  names = TRUE, abbreviate = TRUE, index = TRUE, ref = TRUE,
+  col = "black", linecol = "lightgray", cex = 0.5, pch = 19, xscale = NULL,
+  yscale = c(0, 1), ylines = 1.5)
+{
+    ## node ids
+    node <- nodeids(mobobj, terminal = FALSE)
+
+    ## get all coefficients
+    cf <- apply_to_models(mobobj, node, FUN = function(z)        
+      if(worth) worth(z) else coef(z, all = FALSE, ref = TRUE))
+    cf <- do.call("rbind", cf)
+    rownames(cf) <- node
+
+    ## reference
+    if(ref)
+      cf_ref <- 1/2
+
+    ## labeling
+    if(is.character(names)) {
+      colnames(cf) <- names
+      names <- TRUE
+    }
+    if(is.character(index)) {
+      cf <- cf[, index]  # reorder labels
+      index <- TRUE
+    }
+
+    ## abbreviation
+    if(is.logical(abbreviate)) {
+      nlab <- max(nchar(colnames(cf)))
+      abbreviate <- if(abbreviate) as.numeric(cut(nlab, c(-Inf, 1.5, 4.5, 7.5, Inf))) else nlab
+    }
+    colnames(cf) <- abbreviate(colnames(cf), abbreviate)
+
+    if(index) {
+      x <- 1:NCOL(cf)
+      if(is.null(xscale)) xscale <- range(x) + c(-0.1, 0.1) * diff(range(x))
+    } else {
+      x <- rep(0, length(cf))
+      if(is.null(xscale)) xscale <- c(-1, 1)
+    }
+    if(is.null(yscale)) yscale <- range(cf) + c(-0.1, 0.1) * diff(range(cf))
+
+    ## panel function for mpt plots in nodes
+    rval <- function(node) {
+
+        ## dependent variable setup
+        cfi <- cf[node$nodeID,]
+
+        ## viewport setup
+        top_vp <- viewport(layout = grid.layout(nrow = 2, ncol = 3,
+                           widths = unit(c(ylines, 1, 1), c("lines", "null", "lines")),
+                           heights = unit(c(1, 1), c("lines", "null"))),
+                           width = unit(1, "npc"),
+                           height = unit(1, "npc") - unit(2, "lines"),
+                           name = paste("node_mptplot", node$nodeID, sep = ""))
+        pushViewport(top_vp)
+        grid.rect(gp = gpar(fill = "white", col = 0))
+
+        ## main title
+        top <- viewport(layout.pos.col = 2, layout.pos.row = 1)
+        pushViewport(top)
+        mainlab <- paste(ifelse(id, paste("Node", node$nodeID, "(n = "), ""),
+                         sum(node$weights), ifelse(id, ")", ""), sep = "")
+        grid.text(mainlab)
+        popViewport()
+
+        ## actual plot  
+        plot_vpi <- viewport(layout.pos.col = 2, layout.pos.row = 2,
+            xscale = xscale, yscale = yscale,
+            name = paste("node_mptplot", node$nodeID, "plot", sep = ""))
+        pushViewport(plot_vpi)
+
+        grid.lines(xscale, c(cf_ref, cf_ref), gp = gpar(col = linecol), default.units = "native")
+        if(index) {
+          grid.lines(x, cfi, gp = gpar(col = col, lty = 2), default.units = "native")
+          grid.points(x, cfi, gp = gpar(col = col, cex = cex), pch = pch, default.units = "native")
+          grid.xaxis(at = x, label = if(names) names(cfi) else x)
+        } else {
+          if(names) grid.text(names(cfi), x = x, y = cfi, default.units = "native")
+            else grid.points(x, cfi, gp = gpar(col = col, cex = cex), pch = pch, default.units = "native")
+        }
+        grid.yaxis(at = c(ceiling(yscale[1] * 100)/100, floor(yscale[2] * 100)/100))
+        grid.rect(gp = gpar(fill = "transparent"))
+
+        upViewport(2)
+    }
+
+    return(rval)
+}
+class(node_mptplot) <- "grapcon_generator"
+
